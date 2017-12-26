0d67b86ffe484195e35a9f265a7c5fc68f8a6483
EGOIST <0x142857@gmail.com>
2017-12-26 15:31:16 +0800

    fix sw

--- template/src/index.js
+++ template/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from 'poi-preset-sw-precache/register-service-worker';
+import {AppContainer} from 'react-hot-loader';
 
 const render = Component => {
   ReactDOM.render(
